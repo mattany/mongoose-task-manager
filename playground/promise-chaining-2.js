1ff121d00fca93734cb3b29c02bfada6d2d94b67
myeroushalmi <myeroushalmi@synamedia.com>
2021-08-03 18:19:32 +0300

    added user authentication

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,10 +1,22 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete("6102d069a190c610d0891fef").then(() => {
+// Task.findByIdAndDelete("6102d069a190c610d0891fef").then(() => {
+//     return Task.countDocuments({completed: false})
+// }).then((count) => {
+//     console.log(count)
+// }).catch((e) => {
+//     console.log(e)
+// });
+
+
+const deleteTaskAndCount = async (id) => {
+    await Task.findByIdAndDelete(id)
     return Task.countDocuments({completed: false})
-}).then((count) => {
+}
+
+deleteTaskAndCount("6102eaaa8f50d15470729098").then((count) => {
     console.log(count)
 }).catch((e) => {
     console.log(e)
-});
+})
